7bc7b15c8fd27eee955a27529cf44b491e28f898
mtbishop <bishopmatthew76@gmail.com>
2021-01-26 22:46:31 -0600

    added second API, still needs tweaks

--- script.js
+++ script.js
@@ -102,4 +102,76 @@ $(document).ready(function(){
 
   // call the getBreeds function which will load all the Dog breeds into the dropdown list
   getBreeds();
+
+
+  $("#search-button").on("click", function() {
+
+    $.ajax({
+        url: "https://api.petfinder.com/v2/oauth2/token",
+        method: 'POST',
+        data: {
+            grant_type: "client_credentials",
+            client_id: "czNLzejOZe06Un6VKP0lnnhVn0s0Z0W7sA2m2riGeYNE73Y6oz",
+            client_secret: "Z6hCzpkfaB1XhCqCC4PiokKvAyIAj4bwIQWA2Hsy"
+        },
+        success: dogSearch
+
+    })
+})
+function dogSearch(obj) {
+
+  var zipCode = $("#search-value").val().trim();
+
+$.ajax ({
+    url: "https://api.petfinder.com/v2/organizations?location=" + zipCode,
+    method: 'GET',
+    headers: {
+        "Authorization": obj.token_type + " " + obj.access_token
+    },
+    success: function(result){
+        console.log(result);
+        var arr = result.organizations.splice(0, 10);
+        console.log(arr);
+        var idQuery = arr.map((o,i)=> o.id).join(",");
+        console.log(this);
+        console.log("https://api.petfinder.com/v2/animals" + "?organization=" + idQuery)
+
+        $.ajax ({
+          url: "https://api.petfinder.com/v2/animals" + "?organization=" + idQuery,
+          method: 'GET',
+          headers: {
+            "Authorization": obj.token_type + " " + obj.access_token
+        },
+          success: function(data) {
+            console.log(data.animals);
+            var animals = data.animals;
+            for(i = 0; i < animals.length; i++) {
+              var animal = animals[i];
+              var container = $("<div>").addClass("p-5");
+              var card = $("<div>").addClass("max-w-sm rounded overflow-hidden shadow-lg")
+              var img = $("<img>").attr("src", animal.primary_photo_cropped.small).addClass("w-full").attr("alt", animal.name);
+              // Inner 1
+              var cardInner = $("<div>").addClass("px-6 py-4");
+              var cardTitle = $("<div>").addClass("font-bold text-xl mb-2").text(animal.name);
+              var cardDesc = $("<p>").addClass("text-gray-700 text-base").text(animal.description);
+              // Inner 2
+              var cardInner2 = $("<div>").addClass("px-6 py-4 pb-2");
+              var span1 = $("<span>").addClass("inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2").text(animal.contact.email);
+              var span2 = $("<span>").addClass("inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2").text(animal.contact.phone);
+              var span3 = $("<span>").addClass("inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2").text(animal.breeds.primary);
+              
+              cardInner2.append(span1, span2, span3);
+              cardInner.append(cardTitle, cardDesc);
+              card.append(img, cardInner, cardInner2);
+              container.append(card);
+              $("#dogImg").append(container);
+
+
+            }
+          }
+        })
+
+    }
+})
+}
 })
